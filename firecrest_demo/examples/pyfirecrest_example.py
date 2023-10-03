cd4912ecfbcfdfb64c20380464f2de17198beb7a
James C. Womack <J.C.Womack@bristol.ac.uk>
2023-10-03 16:42:56 +0100

    WIP on exercises in PyFirecREST example

--- firecrest_demo/examples/pyfirecrest_example.py
+++ firecrest_demo/examples/pyfirecrest_example.py
@@ -24,10 +24,63 @@ print(systems)
 
 ## Exercise:
 
-# 1. Get tha different parameters of our deployment
-# 2. Get the username of the user
-# 3. List all microservices and their status
-# 4. List the contents of a directory
-# 5. Upload and download "small" files
+FIRECREST_SYSTEM = os.environ.get("FIRECREST_SYSTEM")
+
+import json
+
+print("1. Get the different parameters of our deployment")
+print(json.dumps(client.parameters(), indent=4))
+
+print("2. Get the username of the user")
+print(json.dumps(client.whoami(machine=FIRECREST_SYSTEM), indent=4))
+
+print("3. List all microservices and their status")
+print(json.dumps(client.all_services(), indent=4))
+
+print("4. List the contents of a directory")
+from pathlib import Path
+work_dir_path = Path("/scratch/snx3000/class414/")
+print(json.dumps(client.list_files(machine=FIRECREST_SYSTEM, target_path=work_dir_path), indent=4))
+
+print("5. Upload and download \"small\" files")
+to_upload_path = Path("./examples/hello_world.txt").resolve(strict=True)
+client.simple_upload(
+    machine=FIRECREST_SYSTEM,
+    source_path=to_upload_path,
+    target_path=work_dir_path,
+)
+
+# ... check file has been uploaded
+files_in_work_dir = client.list_files(machine=FIRECREST_SYSTEM, target_path=work_dir_path)
+filenames = [f["name"] for f in files_in_work_dir]
+print(f"Files in {work_dir_path}: {filenames}")
+print(f"Is {to_upload_path.name} in {work_dir_path} on the remote system?")
+print(to_upload_path.name in filenames)
+
+# ... view contents of file
+file_contents = client.view(
+    machine=FIRECREST_SYSTEM,
+    target_path=work_dir_path / to_upload_path.name
+)
+print("Uploaded file contents:")
+print(file_contents)
+
+# ... download the file
+download_dir_path = (Path.cwd() / "output_tmpdir").resolve()
+download_dir_path.mkdir(exist_ok=True)
+
+source_path = work_dir_path / to_upload_path.name
+target_path = download_dir_path / to_upload_path.name
+
+print(f"Downloading from {source_path} on {FIRECREST_SYSTEM} to {target_path} locally")
+
+client.simple_download(
+    machine=FIRECREST_SYSTEM,
+    source_path=source_path,
+    target_path=target_path,
+)
+
 # 6. Submit a job
+
+
 # 7. [Optional] Submit a job and poll until the it is finished
